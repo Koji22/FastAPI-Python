ff446f9cd64e4bb406fd04c315fd5f1aac2e75b6
Enzo Koji <10403411@mackenzista.com.br>
2025-12-23 15:17:53 -0300

    adicionando migrações com alembic

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,28 @@
+from sqlalchemy import select
+
 from fast_zero.models import User
 
 
-def test_create_user():
+def test_create_user(session):
+    
     user = User(
         username='koji',
         email='koji@mail.com',
-        senha='123')
+        password='123'
+        )
+        
+    session.add(user)
+    session.commit()
 
-    assert user.username == 'koji'
+    result = session.scalar(
+        select(User).where(User.email == 'koji@mail.com')
+    ) #faça um mapeamento e me traga esse objeto
+
+    assert asdict(user) == {
+        'id':1,
+        'username': 'enzo',
+        'password': 'segredo',
+        'email': 'enzo@live.com',
+        'created_at': time,
+        'updated_at': time,
+    }
